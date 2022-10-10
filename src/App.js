b5d75a0fbc26954c86cdca50f9375e40c1c739b8
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-10 13:45:47 -0400

    added some components and setup some routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 
-// import './App.css';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Topics from './components/Topics/Topics';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/header',
+          element: <Header></Header>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <h1>Lets Check</h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
