6cb2cb0cac5c7628fe67509d9e8ae4b3a422ccee
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-12 10:17:42 -0400

    toast added but still working on it

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,30 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 
 const Quiz = ({ quiz }) => {
     console.log(quiz);
     const { question, options, correctAnswer } = quiz;
 
+    const notify = () => toast('Wow, so easy');
+
     return (
         <div>
             <h2>{correctAnswer}</h2>
             <h3 className='px-5 py-2'>Q:{question.slice(3, -4)}</h3>
+            <span><FontAwesomeIcon icon="fa-solid fa-eye" /></span>
             <h5 className='px-5'>
                 {options.map(option =>
                     <div>
                         <input type="radio" id="html" name="fav_language" value="HTML" />
-                        <label>{option}</label>
-
-                        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-                            <div class="toast-header">
-                                <img src="..." class="rounded me-2" alt="..." />
-                                <strong class="me-auto">Bootstrap</strong>
-                                <small>11 mins ago</small>
-                                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-                            </div>
-                            <div class="toast-body">
-                                Hello, world! This is a toast message.
-                            </div>
-                        </div>
+                        <label onClick={notify}>{option}</label>
+                        <ToastContainer />
 
                     </div>)}
 
