6d74828c82232798a0d5a3e804274bd5fc94759e
Anamul10 <anamul.hasan@mnsu.edu>
2022-10-12 12:13:19 -0400

    font awesome eye icon added, toast for right/wrong answer implemented

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,25 +1,39 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React from 'react';
 import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const Quiz = ({ quiz }) => {
     console.log(quiz);
     const { question, options, correctAnswer } = quiz;
 
-    const notify = () => toast('Wow, so easy');
+    const notify = (option) => toast(option === correctAnswer ? 'Your answer is correct' : 'Your answer is wrong', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
 
     return (
-        <div>
-            <h2>{correctAnswer}</h2>
-            <h3 className='px-5 py-2'>Q:{question.slice(3, -4)}</h3>
-            <span><FontAwesomeIcon icon="fa-solid fa-eye" /></span>
-            <h5 className='px-5'>
+        <div >
+            <div className='d-flex justify-content-between pe-5'>
+                <div>
+                    <h3 className='px-5 py-2'>Q:{question.slice(3, -4)}</h3>
+                </div>
+                <div>
+                    <p><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></p>
+                </div>
+            </div>
+            <h5 className='px-5 ms-3'>
                 {options.map(option =>
                     <div>
-                        <input type="radio" id="html" name="fav_language" value="HTML" />
-                        <label onClick={notify}>{option}</label>
-                        <ToastContainer />
-
+                        <button className='p-2 m-2 ' onClick={() => notify(option)}>{option}</button>
+                        <ToastContainer></ToastContainer>
                     </div>)}
 
             </h5>
